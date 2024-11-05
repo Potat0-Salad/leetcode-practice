5e4b97b69f724664cd749e5607644760fa4fa8ee
Potat0-Salad <90644221+Potat0-Salad@users.noreply.github.com>
2024-11-05 12:59:55 +0100

    Time: 0 ms (100%), Space: 15.9 MB (38.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,7 +3,6 @@ public:
     int maxProfit(vector<int>& prices) {
         if (prices.empty()) return 0;
         int minPrice = prices[0];
-        int maxProfit = 0;
         int totalProfit = 0;
 
         for(int i = 1; i < prices.size(); i++){
