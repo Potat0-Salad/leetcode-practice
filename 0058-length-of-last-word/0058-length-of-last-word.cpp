aa829c559f634b765aacf61a06f24021416d035e
Potat0-Salad <90644221+Potat0-Salad@users.noreply.github.com>
2024-12-04 21:48:41 +0100

    Time: 0 ms (100%), Space: 9 MB (9.69%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
+    int lengthOfLastWord(string s){
         int counter = 0;
-        bool toggle = false;
 
-        for(int i = s.length() - 1; i >= 0; i--){
-            cout << i;
-            if(!toggle){
-                if(s[i] == ' '){}
-                else{
-                    counter++;
-                    if(i == 0){
-                        return counter;
-                    }
-                    toggle = true;
-                }
+        for (int i = s.length() - 1; i >= 0; i--){
+            if (s[i] != ' '){
+                counter++;
             }
-            else{
-                if(s[i] != ' '){
-                    counter++;
-                }
-                else{
-                    return counter;
-                }
+            else if (counter > 0){
+                break;
             }
-
         }
         return counter;
     }
