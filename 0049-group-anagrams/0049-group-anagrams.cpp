40dfbdcebffabf10ab26a38f70316fc4be8b321f
Marcel N <90644221+Potat0-Salad@users.noreply.github.com>
2025-11-03 18:27:14 +0100

    Time: 16 ms (61.08%), Space: 26.2 MB (39%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> sorted = strs;
+        unordered_map<string, vector<string>> groups;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            groups[key].push_back(s);
+        }
+
         vector<vector<string>> results;
-        unordered_set<string> done;
-
-        for (string &str : sorted) {
-            sort(str.begin(), str.end());
-        }
-
-        unordered_map<string, vector<string>> map;
-
-        for (int i = 0; i < sorted.size(); i++) {
-            if (done.count(strs[i])) continue; // skip if already grouped
-
-            for (int j = 0; j < sorted.size(); j++) {
-                if (sorted[i] == sorted[j]) {
-                    map[sorted[i]].push_back(strs[j]);
-                    done.insert(strs[j]);
-                }
-            }
-        }
-
-        for (auto pair : map) {
-            string key = pair.first;
-            vector<string> &s = pair.second;
-            vector<string> currentVec;
-            for (auto &val : s) {
-                currentVec.push_back(val);
-            }
-            sort(currentVec.begin(), currentVec.end());
-            results.push_back(currentVec);
+        for (auto& kv : groups) {
+            sort(kv.second.begin(), kv.second.end());
+            results.push_back(kv.second);
         }
 
         return results;
