b0781b78e0fdcc7b61feb6762e66805227c6b411
Marcel N <90644221+Potat0-Salad@users.noreply.github.com>
2026-01-27 19:30:49 +0100

    Time: 4 ms (63.44%), Space: 14.5 MB (26.08%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> rows(numRows, "");
+        int ct = 0;
+        int currRow = 0;
+        bool currDir = true; //true = down, false = up
+        string result;
+
+        if(numRows == 1){
+            return s;
+        }
+
+        while(ct <= s.size() - 1){
+            if(currDir == true && currRow < numRows - 1){
+                rows[currRow]+= s[ct];
+                currRow++;
+            }
+            else if(currDir == true && currRow == numRows - 1){
+                currDir = !currDir; //change direction
+                rows[currRow]+= s[ct];
+                currRow--;
+            }
+            else if(currDir == false && currRow > 0){
+                rows[currRow]+= s[ct];
+                currRow--;
+            }
+            else if(currDir == false && currRow == 0){
+                currDir = !currDir;
+                rows[currRow]+= s[ct];
+                currRow++;
+            }
+            ct++;
+        }
+
+        for(int i = 0; i < rows.size(); i++){
+            result+= rows[i];
+        }
+        return result;
+    }
+};
