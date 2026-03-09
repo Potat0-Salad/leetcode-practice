7200b40e73c039b6ce421f6ccf66045e362cc86f
Marcel N <90644221+Potat0-Salad@users.noreply.github.com>
2026-03-09 15:03:00 +0100

    Time: 2547 ms (5.02%), Space: 55.9 MB (44.72%) - LeetHub

---
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -0,0 +1,51 @@
+class Twitter {
+private:
+    deque<pair<int, int>> feed; //userid, postid
+    unordered_map<int, unordered_set<int>> connections;
+public:
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        feed.push_front({userId, tweetId});
+        cout << "User " << userId << " posted: " << tweetId << endl;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> result;
+        auto end_it = feed.end();
+        auto it = feed.begin();
+
+        cout << "Get news feed for: " << userId << endl;
+
+        while(it != end_it && result.size() < 10){
+            if(connections[userId].contains(it->first) || userId == it->first){
+                result.push_back(it->second);
+                cout << it->second << endl;
+            }
+
+            it++;
+        }
+        return result;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        connections[followerId].insert(followeeId);
+        cout << "User " << followerId << " followed: " << followeeId << endl;
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        connections[followerId].erase(followeeId);
+        cout << "User " << followerId << " un-followed: " << followeeId << endl;
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
